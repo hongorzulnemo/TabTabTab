cef343e03c06d13e9c71c571dae76e978dcb5f04
Jagger <jagarenkhzaya@gmail.com>
2020-10-09 23:31:19 +0200

    Minimal functionality for word_map is working.

--- static/js/helperFunc.js
+++ static/js/helperFunc.js
@@ -1,3 +1,18 @@
 function generateRandomId() {
     return `ITEM_${Math.random().toString().slice(2,10)}`;
 }
+function shuffle_array(input_array) {
+    const output_array = [...input_array];
+    const length = output_array.length;
+    for(let i=0; i<length; i++) {
+        let rand_idx = Math.floor((i)*Math.random());
+        let temp = output_array[i];
+        output_array[i] = output_array[rand_idx];
+        output_array[rand_idx] = temp;
+    }
+    return output_array;
+}
+
+function copy(input_array, num) {
+    return [...input_array].splice(0, num).map(d => {return {...d}})
+}
--- static/js/word_map/word_map.js
+++ static/js/word_map/word_map.js
@@ -1,3 +1,115 @@
-function word_map() {
+function detNumberOfBoxesInEachRow(length) {
+    let powerCounter = 0;
+    let squared = 0;
+    while (length > squared) {
+        powerCounter++;
+        squared = Math.pow(powerCounter, 2);
+    }
+    return powerCounter;
+}
+
+
+function word_map(props) {
+    const {
+        data: data,
+        padFactor: padFactor,
+        svgLength: svgLength
+    } = props;
     
+    const numberOfBoxesInEachRow = detNumberOfBoxesInEachRow(data.length);
+
+    const crossed_array = d3.cross(d3.range(numberOfBoxesInEachRow), d3.range(numberOfBoxesInEachRow));
+    data.forEach(d => {
+        const crossed = crossed_array.shift();
+        d.i = crossed[1];
+        d.j = crossed[0];
+    })
+    const svg = d3.select('#word_map_svg');
+    svg.attr('height', svgLength).attr('width', svgLength).style('background', '#fff');
+    const boxLength = svgLength / numberOfBoxesInEachRow - padFactor;
+    const translationFactor = svgLength / numberOfBoxesInEachRow;
+
+    const translationFunc = (d) => {
+        let i = d.i;
+        let j = d.j;
+        let xDirection = i * translationFactor + padFactor / 2;
+        let yDirection = j * translationFactor + padFactor / 2;
+        return `translate(${xDirection}, ${yDirection})`;
+    };
+
+    const boxTextFunc = (d) => {
+        return d.name;
+    }
+
+    const fillFunc = (d) => {
+        let stage = d.stage;
+        return (stage == 1) ? '#e9c618ab' :
+        (stage == 2) ? '#de425bab' :
+        (stage == 3) ? '#de425b' : '#000';
+    }
+
+
+    const boxStrokeFunc = (d) => {
+        const focused = d.focused;
+        return (focused == true) ? '#000' : '#fff';
+    }
+
+    const boxTextXOffsetFunc = (d) => {
+        return d.name.length*3.5; // needs to scale this!!!
+    }
+
+    const main_ContGroup = new ContainerGroup(svg, 'main_ContGroup');
+    
+    const boxes_GUP = new GeneralUpdatePattern('boxes_GUP', data, 'rect', main_ContGroup.group);
+    boxes_GUP.merge
+        .style('opacity', 1)
+        .attr('height', boxLength)
+        .attr('width', boxLength)
+        .attr('fill', fillFunc)
+        .attr('stroke', d => boxStrokeFunc(d))
+        .attr('stroke-width', 5)
+        .attr('transform', translationFunc);
+    boxes_GUP.exit.remove();
+
+    const box_text = new GeneralUpdatePattern('box_text', data, 'text', main_ContGroup.group); 
+    box_text.merge
+        .attr('x', d => boxLength / 2 - boxTextXOffsetFunc(d))
+        .attr('y', boxLength / 2)
+        .text(d => boxTextFunc(d))
+        .attr('transform', translationFunc);
+    box_text.exit.remove();
+
+
+        
+    const boxes_shell = new GeneralUpdatePattern('boxes_shell', data, 'rect', main_ContGroup.group);
+    boxes_shell.merge
+        .style('fill', '#000')
+        .attr('height', boxLength)
+        .attr('width', boxLength)
+        .attr('opacity', 0)
+        .attr('transform', translationFunc);
+    boxes_shell.merge
+        .on('mouseover', function(i, d) {
+            d3.select(this).style('opacity', 0.5);
+        })
+        .on('mouseout', function(i, d) {
+            d3.select(this).style('opacity', 0);
+        })
+        .on('click', function(i, d) {
+            const selected = copy(ALL_WORDNODES, ALL_WORDNODES.length).filter(wd => wd.id === d.id)[0];
+            console.log(selected)
+            // diagram_data.forEach(dg => {
+            //     if(dg.id === d.id) {
+            //         dg.stage = 2;
+            //     } else {
+            //         dg.stage = 1;
+            //     }
+            // })
+            selected.stage = 2;
+            diagram_data = copy(shuffle_array([...ALL_WORDNODES]), 3);
+            diagram_data = [...diagram_data, selected];
+            // console.log(diagram_data)
+            updateWordMap();
+        })
+    boxes_shell.exit.remove();
 }
