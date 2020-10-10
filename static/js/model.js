5eb2ad8b808f63b8b6c397494aa0ed9c008101fa
Jagger <jagarenkhzaya@gmail.com>
2020-10-10 11:37:51 +0200

    Modified data model. Removed word_two_id. Not needed.

--- static/js/model.js
+++ static/js/model.js
@@ -19,10 +19,9 @@ class Category {
 }
 
 class Category_word {
-    constructor(word_one_id, word_two_id, category_id) {
+    constructor(word_id, category_id) {
         this.id = generateRandomId();
-        this.word_one_id = word_one_id;
-        this.word_two_id = word_two_id;
+        this.word_id = word_id;
         this.category_id = category_id;
         this.stage = 1;
     }
